3d15ed406f2e7520ff51bd73824f5be5bff05253
keith-mikel <keith.mikel@e5sol.com>
2023-05-31 16:30:24 -0500

    Added Js code to save, clear, and apply time color coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,59 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+$(document).ready(function() {
+
+  //header time update by second
+  function updateTime() {
+  var now = dayjs().format('dddd, MMMM D, h:mm:ss A');
+  console.log(now);
+  $("#currentDay").text(now);
+  }
+  updateTime();
+  setInterval(updateTime, 1000);
+
+  //button for clearing local storage and day
+  $('#clearButton').on('click' ,clearButtonDay)
+
+  function clearButtonDay() {
+    localStorage.clear();
+    $("textarea").val("");
+  }
+
+  //iterate accross all hour-items
+  for (var i = 9; i <= 17; i++) {
+    var timeBlockId = "hour-" + i;
+
+    // Load the stored text from local storage and populate the textarea
+    var storedText = localStorage.getItem(timeBlockId);
+    if (storedText) {
+      $("#" + timeBlockId + " textarea").val(storedText);
+    }
+
+
+    $("#" + timeBlockId + " .saveBtn").on("click", createSaveButtonHandler(timeBlockId));
+  }
+
+  // Save button click event handler
+  function createSaveButtonHandler(timeBlockId) {
+    return function() {
+      var text = $("#" + timeBlockId + " textarea").val();
+      localStorage.setItem(timeBlockId, text);
+    } 
+}
+
+var currentHour = dayjs().format('hA');
+  for (var i = 9; i <= 17; i++) {
+    var timeBlockId = "hour-" + i;
+    if (currentHour === $("#" + timeBlockId + " .hour").text()) {
+      $("#" + timeBlockId + " textarea").addClass("present");
+    }
+    else if (currentHour > $("#" + timeBlockId + " .hour").text()) {
+      $("#" + timeBlockId + " textarea").addClass("past");
+    }
+    else {
+      $("#" + timeBlockId + " textarea").addClass("future");
+    }
+    
+
+}
+
+}
+);
